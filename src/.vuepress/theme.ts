15273961dbf647b2f0e060becb99fd23374a92ad
AvalonC <ugz2000@live.com>
2023-09-18 22:20:03 +0800

    MIS Change sync

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -6,7 +6,7 @@ export default hopeTheme({
   hostname: "https://vuepress-theme-hope-docs-demo.netlify.app",
 
   author: {
-    name: "Mr.Hope",
+    name: "MScBIS",
     url: "https://mister-hope.com",
   },
 
