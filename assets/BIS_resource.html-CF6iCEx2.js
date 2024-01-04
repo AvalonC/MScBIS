1d9055a8ef2c46243266be49d88d3af72e5028d9
AvalonC <57788292+AvalonC@users.noreply.github.com>
2024-01-04 10:43:34 +0000

    Deploying to gh-pages from @ AvalonC/MScBIS@17ee2cfbe85911d0110b5d84e2c38477428761e2 🚀

---
+++ assets/BIS_resource.html-CF6iCEx2.js
@@ -0,0 +1,18 @@
+import{_ as i}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as s,o as a,c as d,b as e,d as n,a as r,f as t}from"./app-dbDRtNsi.js";const c={},o=e("p",null,"此为BIS常用资源站",-1),u=e("h2",{id:"使用规约",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#使用规约","aria-hidden":"true"},"#"),n(" 使用规约")],-1),v=e("p",null,[e("strong",null,"网盘内资料仅供BIS内部使用，严禁外传、商用、非法传播，一经发现将追究相关法律责任。")],-1),m=e("thead",null,[e("tr",null,[e("th",null,"地址"),e("th",null,"密码")])],-1),_={href:"https://pan.baidu.com/s/1kecaegEXynazWMu2I0N4fA",target:"_blank",rel:"noopener noreferrer"},h=e("td",null,"hxk2",-1),b=t(`<h2 id="网盘结构" tabindex="-1"><a class="header-anchor" href="#网盘结构" aria-hidden="true">#</a> 网盘结构</h2><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>|-- 实用文件
+    |-- 粤语
+        |-- 粤语课录屏
+    |-- 求职
+        |-- 内陆
+        |-- 留港
+        |-- 就业讲座
+        |-- 笔面资料
+    |-- 教材
+    |-- 攻略
+|-- BIS
+    |-- 时间
+        |-- 22 Fall
+        |-- 21 Fall及之前
+    |-- 方向
+        |-- MIS
+        |-- FIT
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2);function f(p,x){const l=s("ExternalLinkIcon");return a(),d("div",null,[o,u,v,e("table",null,[m,e("tbody",null,[e("tr",null,[e("td",null,[e("a",_,[n("百度网盘"),r(l)])]),h])])]),b])}const B=i(c,[["render",f],["__file","BIS_resource.html.vue"]]);export{B as default};
