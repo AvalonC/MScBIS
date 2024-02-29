782f30e1a5d7b17dbe24a24e1188d4f858830a02
AvalonC <57788292+AvalonC@users.noreply.github.com>
2024-02-29 03:44:20 +0000

    Deploying to gh-pages from @ AvalonC/MScBIS@7f78d74ecc9b94a8e8477e3bc1c38db7240c1fde 🚀

---
+++ assets/about.html-rsDRA-PV.js
@@ -0,0 +1,9 @@
+import{_ as a}from"./plugin-vue_export-helper-x3n3nnut.js";import{r as s,o as i,c as r,a as e,b as o,w as l,e as d,d as t}from"./app-BE8nK2OH.js";const u={},c=e("h2",{id:"作者",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#作者","aria-hidden":"true"},"#"),t(" 作者")],-1),h=e("p",null,[e("em",null,"MSc - Business Information Systems (City University of Hong Kong)")],-1),_=e("p",null,[e("em",null,"BMgt - Information Management and Information Systems (Kunming University of Science and Technology)")],-1),m=e("table",null,[e("thead",null,[e("tr",null,[e("th",null,"Period"),e("th",null,"Name"),e("th",null,"Position")])]),e("tbody",null,[e("tr",null,[e("td",null,"2022.08-2023.10"),e("td",null,"MScBIS Programme"),e("td",null,"Class representative")])])],-1),f=e("p",null,"Welcome to this site! I hope the data provided by this site is helpful!",-1),p=e("h2",{id:"联系方式",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#联系方式","aria-hidden":"true"},"#"),t(" 联系方式")],-1),g=e("p",null,[e("a",{href:"mailto:Guanzhang.WANG@my.cityu.edu.hk"},"点此进行邮件联系，仅限CityU邮箱账户")],-1);function b(y,v){const n=s("font");return i(),r("div",null,[c,e("p",null,[e("strong",null,[o(n,{color:"#b01861"},{default:l(()=>[t('汪冠彰 (Mr. WANG Guanzhang) "Steve"')]),_:1})])]),h,_,e("p",null,[e("strong",null,[o(n,{color:"#b01861"},{default:l(()=>[t("Administrative Assignments")]),_:1})])]),m,f,d(` 
+## 关于双站更新
+
+为防止各种问题的产生，本站有两个站点，分别由GitHub Pages（国际站）与Gitee Pages（国内站）进行托管。
+
+两站之间的差异如下：
+
+1. 国内站更新会不如国际站及时。
+2. 国内站的更新需要手动到Gitee操作，因此很可能出现国际站领先好几个版本的问题，**所以一定要以国际站信息为准。** `),p,g])}const N=a(u,[["render",b],["__file","about.html.vue"]]);export{N as default};
